625da73978a6f531114405803894e30aed723a1a
Ragupathi-22 <eragupathi06@gmail.com>
2024-12-27 16:20:26 +0530

    revert ragu feature

--- test_git.php
+++ test_git.php
@@ -3,8 +3,11 @@
    1  new feature  added
    2 feature added
 <<<<<<< HEAD
+<<<<<<< HEAD
    3 feature added by krishna
 =======
    3 feature added by ragu
 >>>>>>> c90b35ff0690ce435c27eddc48ba8123780c7178
+=======
+>>>>>>> parent of c90b35f (3 feature add by ragu)
 ?>
