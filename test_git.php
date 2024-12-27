6a518b47f1aa9073d521452024e4e1936ec45180
Ragupathi-22 <eragupathi06@gmail.com>
2024-12-27 16:47:27 +0530

    4th krishna

--- test_git.php
+++ test_git.php
@@ -2,9 +2,5 @@
    new file
    1  new feature  added
    2 feature added
-<<<<<<< HEAD
-   3 feature added by krishna
-=======
-   3 feature added by ragu
->>>>>>> c90b35ff0690ce435c27eddc48ba8123780c7178
+   4 feature added by krishna
 ?>
